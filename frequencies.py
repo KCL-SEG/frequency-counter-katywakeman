47ee8cbef644c49a5d4be3a46b0f77df1f0bfe1f
katywakeman <124585957+katywakeman@users.noreply.github.com>
2023-10-02 20:18:49 +0100

    Add frequencies.py functionality

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    n = len(items)
+    count = 0
+    for i in range(n):
+      for j in range(0, n - i - 1):
+          if string(items[i]) == string(items[j]):
+            count = count + 1
+      frequencies[i] = count
     return frequencies
