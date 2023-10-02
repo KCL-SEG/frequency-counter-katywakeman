d79c55046d9749a629092abce60afa41511f9d50
katywakeman <124585957+katywakeman@users.noreply.github.com>
2023-10-02 20:41:41 +0100

    Fix count and duplication

--- frequencies.py
+++ frequencies.py
@@ -2,13 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    duplicates = {}
     frequencies = {}
     # Your code goes here
     n = len(items)
     count = 0
     for i in range(n):
-      for j in range(0, n - i - 1):
+      for j in range(n):
           if str(items[i]) == str(items[j]):
             count = count + 1
-      frequencies[i] = count
+      duplicates[str(items[i])] = count
+      count = 0
+      for key,value in duplicates.items():
+        if value not in frequencies.values():
+          frequencies[key] = value
     return frequencies
